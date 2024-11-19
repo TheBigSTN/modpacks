4ad522700ea1b4164c9fc87ae5cc603a32456a52
DESKTOP-M1U45KI\stans <stan.stelian08@gmail.com>
2024-11-19 20:05:26 +0200

    Revert "added version 1.0.0 of immersivecreate"
    This reverts commit 58baaacb0d7709241a4b3d35c2c5b096f15f86d9.

--- immersivecrafting-@1.0.0/kubejs/server_scripts/Tags.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.tags("item", event => {
-    event.add("createbigcannons:ingot_bronze","create:brass")
-    event.add("createbigcannons:block_bronze","create:brass_block")
-    event.add("sliceanddice:allowed_tools","minecraft:shears")
-})
--- immersivecrafting-@1.0.0/kubejs/startup_scripts/fluids.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.registry('fluid', event => {
-    event.create('liquid_redstone')
-        .thinTexture(0xAA0000)
-        .bucketColor(0xAA0000)
-        .displayName('Liquid Redstone')
-})
